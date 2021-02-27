84d6e858df5e9a94391b6c47e314cd22b8d8038a
Alice Lieutier <alice.lieutier@gmail.com>
2021-02-27 19:32:07 +0000

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+function moonPhase(date) {
+  const synodicMonthInMs = 2551442878
+  const aNewMoon = new Date('11 Feb 2021 19:05').getTime()
+  const now = date.getTime()
+  const moment = ((now - aNewMoon + 86450000) % synodicMonthInMs) / synodicMonthInMs 
+  const phases = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"]
+  return phases[Math.floor(moment * 8)];
+}
+
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function greet() {
+  const greetings = ["Bonjour", "Salut", "Hello", "Hi", "Hola", "Hallo", "Selam"]
+  return sample(greetings)
+}
+
+document.getElementById('moon').innerText = moonPhase(new Date())
+
+function displayGreeting() {
+  document.getElementById('greeting').innerText = greet()
+  setTimeout(displayGreeting, 7000)
+}
+
+displayGreeting()
